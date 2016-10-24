ebeba1baf378e2f61d60ff2a1735e2bb13d2a628
platypusrex <frankcooke79@gmail.com>
2016-10-24 00:51:12 -0400

    fixing the whole project :)

---
+++ src/js/model.js
@@ -0,0 +1,34 @@
+import moment from 'moment';
+
+export default class Model {
+	constructor() {
+		this.now = moment().day(15);
+	}
+
+	setData(month, year) {
+		this.now.month(month).year(year);
+	}
+
+	getDays() {
+		const days = [];
+		const calendarStart = moment(this.now).startOf('month');
+		const calendarEnd = moment(this.now).endOf('month');
+		const timeRange = calendarEnd.valueOf() - calendarStart.valueOf();
+		const daysInView = Math.floor(moment.duration(timeRange).asDays());
+
+		for(let i = 0; i <= daysInView; i++) {
+			days.push({
+				iso: moment(calendarStart).add(i, 'days').toISOString()
+			});
+		}
+
+		return days;
+	}
+
+	toJSON() {
+		const iso = this.now.toISOString();
+		const days = this.getDays();
+
+		return {iso, days};
+	}
+}
